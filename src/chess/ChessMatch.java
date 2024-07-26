570a293f33b88f561205e839e5ed5912f460416e
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2024-07-26 16:31:55 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -47,10 +47,17 @@ public class ChessMatch {
         placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
@@ -64,11 +71,19 @@ public class ChessMatch {
 		}
 	}
 	
+	private void validateTargetPosition(Position source, Position Target) {
+		if(!board.piece(source).possibleMove(Target)) {
+			throw new ChessException("The chosen piece can't move to the target position!");
+		}
+	}
+	
 	private Piece makeMove(Position source, Position target) {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		return capturedPiece;
 	}
+	
+	
 	
 }
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,15 +1,16 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece{
+public class King extends ChessPiece {
 
 	public King(Board board, Color color) {
-		super(board, color);	
+		super(board, color);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "K";
@@ -18,6 +19,9 @@ public class King extends ChessPiece{
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		
+
 		return mat;
 	}
 
